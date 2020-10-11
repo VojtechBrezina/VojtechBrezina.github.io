0e14f86c5a0220892acfe4600ad6f6bd446837fa
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-10-11 12:00:49 +0200

    .

--- quiz.js
+++ quiz.js
@@ -1,7 +1,16 @@
+class Text{
+    static global_X = 0;
+    static text_height = 80;
+    constuctor(text, typ){
+
+    }
+}
+
 function setup(){
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-    background(100);
+    background(255);
+
 }
